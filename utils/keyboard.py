ab50cad9c22afe02fcc70c93be90ee797b4d0ecc
edminar <edmin0312@gmail.com>
2024-06-11 21:45:29 +0300

    feat: исправила баг с подключением LLM

--- utils/keyboard.py
+++ utils/keyboard.py
@@ -4,7 +4,7 @@ def dynamic_keyboard(options):
     keyboard = InlineKeyboardMarkup(row_width=3)
     buttons = [InlineKeyboardButton(text=opt, callback_data=f"choice_{idx}") for idx, opt in enumerate(options, start=1)]
     keyboard.add(*buttons)
-    return keyboar
+    return keyboard
 
 def pagination_keyboard(total_pages, current_page):
     keyboard = InlineKeyboardMarkup()
